e69ffd674637410fdf60a179e1246c7bda97767e
ananyaJoshi2003 <77530072+ananyaJoshi2003@users.noreply.github.com>
2025-06-28 15:21:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // Configure CORS middleware
 app.use(
   cors({
-    origin: ["https://test-front-flame.vercel.app", "http://localhost:3000"],
+    origin: ["https://test-front-flame.vercel.app", "http://localhost:3000","https://www.kartalucia.com"],
     credentials: true,
   })
 ); // Allow all CORS requests by default
